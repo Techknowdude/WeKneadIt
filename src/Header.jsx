76a4bbcf619170a6f3863a64f45634ea0f256bf6
Brandon Westmoreland <gamingking@gmail.com>
2026-08-24 13:36:37 -0700

    Header work - BG and logo positioning

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,24 @@
 import { useContext } from "react";
 import { Link } from "@tanstack/react-router";
 // import { CartContext } from "./contexts";
+import Background from "./images/WeKneadIt Customer Header.png";
+import Logo from "./images/WeKneadIt logo - white.png";
 
 export default function Header() {
   //   const [cart] = useContext(CartContext);
   return (
-    <nav className="flex bg-header-color">
-      <Link to={"/"} className="">
-        <h1 className="text-4xl">WeKneadIt</h1>
+    <nav className="bg-header-color relative w-full h-100">
+      <img src={Background} className="w-full h-full object-fill" />
+      <Link
+        to={"/"}
+        className="absolute inset-0 flex items-start justify-center object-cover"
+      >
+        <img src={Logo} className="absolute flex w-1/4" />
       </Link>
-      <div className="ml-auto text-3xl text-black">🛒</div>
+      <div className="absolute inset-0 flex items-start justify-end mr-auto text-xl text-black">
+        🛒
+      </div>
+      {/* Search bar */}
     </nav>
   );
 }
